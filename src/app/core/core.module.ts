6ff2ac27be1634f6a0a7801821a87c5c70abbc80
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-07 16:26:04 +0300

    - Generate and export component 'GoToComponent'.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/navs/header/header.component';
 import { FooterComponent } from './components/navs/footer/footer.component';
+import { GoToComponent } from './components/go-to/go-to.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, GoToComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, GoToComponent]
 })
 export class CoreModule { }
