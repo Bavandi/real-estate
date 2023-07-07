1f4028fbc6d357a493359a124207d7c978e2cf9b
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-07 16:29:28 +0300

    - Declare and create method '_initializeGoTo'.

--- src/app/core/components/go-to/go-to.component.ts
+++ src/app/core/components/go-to/go-to.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+declare const HSGoTo: any;
+
 @Component({
   selector: 'bre-go-to',
   templateUrl: './go-to.component.html',
@@ -11,6 +13,13 @@ export class GoToComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private _initializeGoTo() {
+    // INITIALIZATION OF GO TO
+    // =======================================================
+    new HSGoTo('.js-go-to');
+
   }
 
 }
