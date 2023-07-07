b739761c19a0ca2ee047c979243b3d43c247e5cc
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-07 16:26:45 +0300

    - Change 'changeDetection' to 'OnPush'.

--- src/app/core/components/go-to/go-to.component.ts
+++ src/app/core/components/go-to/go-to.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bre-go-to',
   templateUrl: './go-to.component.html',
-  styleUrls: ['./go-to.component.css']
+  styleUrls: ['./go-to.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GoToComponent implements OnInit {
 
