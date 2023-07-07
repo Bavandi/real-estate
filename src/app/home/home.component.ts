8ddead40de58cbb9a7e703d2f9f519379532920d
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-07 16:31:34 +0300

    - Declare constant 'HSCore'. - Create private method '_initializeTextAnimation'.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+declare const HSBsValidation: any;
+
+declare const HSCore: any;
+
 @Component({
   selector: 'bre-home',
   templateUrl: './home.component.html',
@@ -11,6 +15,23 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  private _initializationOfBootstrapValidation() {
+    // INITIALIZATION OF BOOTSTRAP VALIDATION
+    // =======================================================
+    HSBsValidation.init('.js-validate', {
+      onSubmit: (data: any) => {
+        data.event.preventDefault()
+        alert('Submited')
+      }
+    })
+  }
+
+  private _initializeTextAnimation() {
+    // INITIALIZATION OF TEXT ANIMATION (TYPING)
+    // =======================================================
+    HSCore.components.HSTyped.init('.js-typedjs')
   }
 
 }
