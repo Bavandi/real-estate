46753dabd661a8a54405b7e42b7bee2bf10775c6
Karangu Wairore Lucas <kwairore@gmail.com>
2023-07-07 16:07:44 +0300

    - Generate and export component 'FooterComponent'.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/navs/header/header.component';
+import { FooterComponent } from './components/navs/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class CoreModule { }
